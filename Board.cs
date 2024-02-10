76fe672f70075794275aa29cf32f30b32444e35a
minjun Kim <k980128@pusan.ac.kr>
2024-02-11 00:31:11 +0900

    123

---
+++ Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1_1
+{
+    internal class Board
+    {
+        public int[] _data = new int[25];//배열 , 연속된 방을 사용한다
+        public List<int> _data2 = new List<int>();//동적 배열 >>처음부터 여유있게 배열칸을 보유, 유동적으로 활용가능
+        public LinkedList<int> _data3 = new LinkedList<int>();//연결 리스트
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
 ﻿
+////
 Console.CursorVisible = false;
+
+int LastTick = 0;
+const int WaitTick = 1000 / 30;
+const char Circle = '\u25cf';
+
 while (true)
 {
+    #region FrameContr
+    int currentTick = System.Environment.TickCount;//gets the current tick count
+    int elapsedtick = currentTick - LastTick;
+
+    //elapsedtick == 경과한 시간
+    if (elapsedtick < WaitTick) //만약 경과한 시간이 1/30초보다 작다면 >> tickcount가 밀리세컨드이기에>>1000/30
+        continue;
+    LastTick = currentTick;
+    #endregion
+
     Console.SetCursorPosition(0, 0);
-    Console.WriteLine("hello world!");
+    
+    for(int i=0; i < 25; i++)
+    {
+        for (int j = 0; j < 25; j++)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(Circle);
+        }
+        Console.WriteLine();
+    }
+       
 }
