f4587570af6d6778cca666ed6adbca448d2b7bf9
kim minjun <k980128@naver.com>
2024-02-18 23:37:30 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,88 @@
-﻿
-////
-using class1_1;
+﻿using System;
+using System.Collections.Generic;
 
-///
-Board board = new Board();
-Player player = new Player();   
-board.Initialize(25,player);
-player.initialize(1, 1, board);
-Console.CursorVisible = false;
-
-int LastTick = 0;
-const int WaitTick = 1000 / 30;
- 
-
-while (true)
+namespace Learning1
 {
-    #region FrameContr
-    int currentTick = System.Environment.TickCount;//gets the current tick count
-    int elapsedtick = currentTick - LastTick;
+    //스택 : LIFO>>후입선출
+    //큐 : FIFO >>선입선출
 
-    //elapsedtick == 경과한 시간
-    if (elapsedtick < WaitTick) //만약 경과한 시간이 1/30초보다 작다면 >> tickcount가 밀리세컨드이기에>>1000/30
-        continue;
-    int deltaTick = currentTick - LastTick;
-    LastTick = currentTick;
-    #endregion
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            {0, 1, 0, 1, 0, 0, },
+            {1, 0, 1, 1, 0, 0, },
+            {0, 1, 0, 0, 0, 0, },
+            {1, 1, 0, 0, 0, 0, },
+            {0, 0, 0, 0, 0, 1, },
+            {0, 0, 0, 0, 1, 0, },
+        };
 
-    //입력
+        List<int>[] adj2 = new List<int>[]
+        {
+            new List<int>(){ 1, 3 },
+            new List<int>(){ 0, 2, 3 },
+            new List<int>(){ 1 },
+            new List<int>(){ 0, 1 },
+            new List<int>(){  5 },
+            new List<int>(){ 4 },
+        };
 
-    //로직
-    player.Update(deltaTick);
+        // 1.now 부터 시작하고
+        // 2. now와 연결된 점들을 하나씩 확인, 미방문 상태라면 방문
+        bool[] _visited = new bool[6];
+        public void DFS(int now)
+        {
+            Console.WriteLine(now);
+            _visited[now] = true; // 방문했음을 표시
 
-    //렌더링
-    Console.SetCursorPosition(0, 0);
-    board.Render();
-    
-    
-       
+            for ( int next = 0; next < 6; next++)
+            {
+                if(adj[now, next] == 0)//연결되어 있지 않을때, 스킾       
+                    continue;
+                if (_visited[next])//다음갈 노드가 이미 방문완료일때, 스킾
+                    continue;
+                DFS(next); //(재귀함수) 위 두개가 해당 사항이 아닐때, 다음 노드에서 시작
+            }
+        }
+
+        public void DFS2(int now)
+        {
+            Console.WriteLine(now);
+            _visited[now] = true; // 방문했음을 표시
+
+            foreach (int next in adj2[now])
+            {
+                //이미 연결된 정보만 다루기에 연결이 안되어있는 상황 배제
+                if (_visited[next])//다음갈 노드가 이미 방문완료일때, 스킾
+                    continue;
+                DFS2(next); //(재귀함수) 위 조건이 해당 사항이 아닐때, 다음 노드에서 시작
+            }
+        }
+
+        public void SearchAll()
+        {
+            _visited = new bool[6];
+            for (int now = 0; now < 6; now++)
+            {
+                if (_visited[now] == false)
+                    DFS2(now);
+            }
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // DFS : 깊이 우선 탐색
+            Graph graph = new Graph();
+            //graph.DFS(5);
+            //graph.DFS2(5);
+            graph.SearchAll();
+
+            // BFS : 너비 우선 탐색
+
+        }
+    }
 }
