2120efeca117a0cd3a44061ec4c842ed33b6614d
minjun Kim <k980128@pusan.ac.kr>
2024-02-20 03:25:30 +0900

    BFS learning

--- BFS-DFS/Program.cs
+++ BFS-DFS/Program.cs
@@ -73,10 +73,14 @@ namespace Learning1
         public void BFS(int start)
         {
             bool[] found = new bool[6];
+            int[] parent = new int[6];
+            int[] distance = new int[6];
 
             Queue<int> q = new Queue<int>();
             q.Enqueue(start);
             found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;
 
             while (q.Count > 0) //q 대기열에 하나라도 있을떄 계속 반복
             {
@@ -91,7 +95,9 @@ namespace Learning1
                         continue;
                     q.Enqueue(next);//연결은 되어 있지만, 방문하지 않았을 경우, 예약 목록에 넣어줌
                     found[next] = true; //방문 했음을 표시
-
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1; //이전에서 now 까지의 거리에서 +1
+                    //이렇게 경로를 거치면 많은 정보를 얻을수 있다
                 }
             }
 
@@ -104,12 +110,12 @@ namespace Learning1
         {
             Graph graph = new Graph();
 
-            // DFS : 깊이 우선 탐색
+            // DFS : 깊이 우선 탐색 >> 다양한 용도 사용
             //graph.DFS(5);
             //graph.DFS2(5);
             //graph.SearchAll(); //노드 연결이 끊겨 있을때도 전부 확인 하는 함수 
 
-            // BFS : 너비 우선 탐색
+            // BFS : 너비 우선 탐색 >> 최단거리 길찾기 특화
             graph.BFS(0);
 
         }
